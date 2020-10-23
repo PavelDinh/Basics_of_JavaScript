bc90c86f4296a9fee8608ce6bf71b5a348fdbf6f
Vu Dinh Hoang <32274627+PavelDinh@users.noreply.github.com>
2020-10-23 09:04:23 +0200

    Functions, Arrays, GlobalVars, If

--- src/index.js
+++ src/index.js
@@ -68,3 +68,147 @@ console.log(myStr + "\\ first letter of this sentence is: '" + myStr[0] + "'");
 // We cannot change individual letter in a string
 // myStr[0] = "B" <- Not Working !
 // You need to change the whole string
+
+var gettingLastLetterFromMyStr = myStr[myStr.length - 1];
+console.log(gettingLastLetterFromMyStr);
+
+// FUNCTIONS
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Code logic
+  var result = "";
+  result +=
+    "The " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " to the store " +
+    myAdverb +
+    ".";
+
+  // Optional return type
+  return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+// Arrays
+var ourArray = ["John", 23, "a", 234.65]; // CONFUSING !
+var myArr = ["Quancy", 1];
+var twoDArr = [
+  ["Bulls", 25],
+  ["White socks", 23],
+  ["Cow", 345]
+];
+
+// Accessing 2D array
+console.log(twoDArr[1][0]);
+
+// Adding data to the end of an array
+twoDArr.push(["happy", "Paul"]);
+console.log(twoDArr[2]);
+
+// Adding data to the start of an array
+twoDArr.unshift(["Many", "Sadness"]);
+
+// Removing data from the end of an array
+twoDArr.pop();
+console.log(twoDArr[2]); // Undefined is right because we deleted the element
+
+// Removing data from the beginning of an array
+console.log(twoDArr[0]);
+twoDArr.shift();
+console.log(twoDArr[0]);
+
+// Accessing arrays index
+var ourData = ourArray[0];
+console.log(ourData);
+
+// Arrays indexes can be modified
+var myArray = [12, 4325, 3456, 23];
+myArray[2] = 1;
+
+// Shopping list
+var myList = [
+  ["cereal", 3],
+  ["milk", 2],
+  ["bananas", 3],
+  ["juice", 2],
+  ["eggs", 4]
+];
+
+function reusableFunc() {
+  console.log("Hi Cutie !");
+}
+
+reusableFunc();
+
+// SCOPES
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to 'oopsGlobal'
+  var oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += "oopsGlobal: " + oopsGlobal; // Out of scope
+  }
+  console.log(output);
+}
+
+fun1();
+fun2();
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater"; // Taking precedence
+  return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);
+
+// Stand in Line
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift();
+}
+
+var testArr = [1, 2, 3, 4, 5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+// Comparing IF statements
+/**
+ * 3 == 3 -> true
+ * 3 == '3' -> true
+ * 3 === 3 -> true
+ * 3 === '3' -> false
+ */
+
+function compareEquality(a, b) {
+  if (a == b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+function strictCompare(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log(compareEquality(10, "10"));
+console.log(strictCompare(10, "10"));
